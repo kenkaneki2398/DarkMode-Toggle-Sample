d8685474bb11e06d068ee093151e659e90f7a86e
kenkaneki <lunafreya2398@gmail.com>
2021-02-24 21:12:18 +0700

    Completed toggle between default and dark mode

--- src/App.js
+++ src/App.js
@@ -2,8 +2,48 @@ import React, { useState, useEffect } from 'react'
 import data from './data'
 import Article from './Article'
 
+const getStorageTheme = () => {
+  let theme = 'light-theme'
+  if(localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme')
+  }
+  return theme
+}
+
 function App() {
-  return <h2>dark mode starter</h2>
+  const [theme, setTheme] = useState(getStorageTheme())
+  const [isDarkMode, setIsDarkMode] = useState(false)
+
+  const toggleTheme = () => {
+    if(theme === 'light-theme') {
+      setTheme('dark-theme')
+      setIsDarkMode(true)
+    } else {
+      setTheme('light-theme')
+      setIsDarkMode(false)
+    }
+  }
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>Dark Mode Sample</h1>
+          <button className="btn" onClick={toggleTheme}>{isDarkMode ? 'Turn on the light' : 'Turn off the light'}</button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((article) => {
+          return <Article key={article.id} {...article} />
+        })}
+      </section>
+    </main>
+  )
 }
 
 export default App
--- src/Article.js
+++ src/Article.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
+const Article = ({ title, date, snippet }) => {
+  return (
+    <article>
+      <h2>{title}</h2>
+      <div>
+        <span>{moment(date).format('DD/MM/YYYY')}</span>
+      </div>
+      <p>{snippet}</p>
+    </article>
+  )
 }
 
 export default Article
