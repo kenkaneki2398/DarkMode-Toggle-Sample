1671e95ece00d07799b747dc3ba822d62d8b4af9
kenkaneki <lunafreya2398@gmail.com>
2021-02-25 19:26:40 +0700

    Project setup

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const articles = [
+    {
+      id: 1,
+      title: 'Facebook thay đổi thái độ với Australia',
+      date: new Date(2021, 2, 23),
+      snippet: `Chưa đầy một tuần sau khi ngừng hiển thị tin tức từ Australia, Facebook đã rút lại quyết định trên.`,
+    },
+    {
+      id: 2,
+      title: 'Huawei ra mắt smartphone gập mới, giống Galaxy Z Fold2',
+      date: new Date(2021, 2, 23),
+      snippet: `Huawei Mate X2 sở hữu 2 màn hình, phần viền được làm mỏng.`,
+    },
+    {
+      id: 3,
+      title: 'iPhone Flip có thể ra mắt năm 2024?',
+      date: new Date(2021, 2, 21),
+      snippet: `Tin đồn cho biết mẫu smartphone màn hình gập đầu tiên của Apple sẽ ra mắt sớm nhất vào năm 2024.`,
+    },
+    {
+      id: 4,
+      title: 'Linux lần đầu được chạy trên Hỏa tinh',
+      date: new Date(2021, 2, 21),
+      snippet: `Cùng với tàu thăm dò Perseverance, trực thăng Ingenuity sẽ ghi lại hình ảnh bề mặt Hỏa tinh phục vụ nghiên cứu.`,
+    },
+  ]
+  
+  export default articles
